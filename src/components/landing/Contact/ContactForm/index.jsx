0539ad036d1c717e5f2267663f69a09b656ac465
Mitch Lum <mklum88@gmail.com>
2020-07-10 17:06:04 -0700

    remove recaptcha

--- src/components/landing/Contact/ContactForm/index.jsx
+++ src/components/landing/Contact/ContactForm/index.jsx
@@ -6,8 +6,6 @@ import * as Yup from 'yup';
 import { Button, Input } from 'components/common';
 import { Error, Center, InputField } from './styles';
 
-const recaptchaRef = React.createRef();
-
 export default () => (
   <Formik
     initialValues={{
@@ -23,7 +21,7 @@ export default () => (
         .email('Invalid email')
         .required('Email field is required'),
       message: Yup.string().required('Message field is required'),
-      recaptcha: Yup.string().required('Robots are not welcome yet!'),
+      // recaptcha: Yup.string().required('Robots are not welcome yet!'),
     })}
     onSubmit={async ({ name, email, message }, { setSubmitting, resetForm, setFieldValue }) => {
       try {
@@ -92,8 +90,8 @@ export default () => (
         </InputField>
         {/* values.name && values.email && values.message && (
           <InputField>
-            <Recaptcha
-              ref={recaptchaRef}
+            <FastField
+              component={Recaptcha}
               sitekey={process.env.GATSBY_PORTFOLIO_RECAPTCHA_KEY}
               name="recaptcha"
               onChange={value => setFieldValue('recaptcha', value)}
